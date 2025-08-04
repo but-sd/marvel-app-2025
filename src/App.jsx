a4f607d66bba363d21c90cb51ebeebfc12543e52
alexandre-girard <alexandre.girard@maif.fr>
2025-08-04 16:24:08 +0200

    Refactor App structure and styles: remove old HTML and CSS files, add character list to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import './App.css'
 
 function App() {
-  return (<h1>Hello World from react with JSX</h1>);
+  return (
+    <>
+      <h1>Marvel Characters</h1>
+      <ul id="characters">
+        <li>
+          Beast
+        </li>
+        <li>
+          Captain America
+        </li>
+        <li>
+          Deadpool
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default App;
