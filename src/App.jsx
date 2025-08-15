4be8c7e6b7d0683da45efa31633827c2c1bfbb91
alexandre-girard <alexandre.girard@maif.fr>
2025-08-15 10:30:07 +0200

    init pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import './App.css'
 import CharactersList from './components/CharactersList';
 import NumberOfCharacters from './components/NumberOfCharacters';
 import characters from './data/characters.json';
+import AboutPage from './pages/AboutPage';
+import CharactersPage from './pages/CharactersPage';
+import ContactPage from './pages/ContactPage';
 
 function App() {
   return (
     <>
-      <h1>Marvel Characters</h1>
+      {/* <AboutPage /> */}
+      {/* <ContactPage /> */}
+      <CharactersPage />
+
+      {/* <h1>Marvel Characters</h1>
       <CharactersList characters={characters} />
       <br/>
-      <NumberOfCharacters characters={characters} />
+      <NumberOfCharacters characters={characters} /> */}
     </>
   );
 }
