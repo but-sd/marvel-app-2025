9007aac9f91a82a21240f1143352a1399a24b5af
alexandre-girard <alexandre.girard@maif.fr>
2025-08-15 11:01:22 +0200

    Refactor App component to use Layout and implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import './App.css'
-import CharactersList from './components/CharactersList';
-import NumberOfCharacters from './components/NumberOfCharacters';
-import characters from './data/characters.json';
+import { BrowserRouter, Route, Routes } from "react-router";
 import AboutPage from './pages/AboutPage';
 import CharactersPage from './pages/CharactersPage';
 import ContactPage from './pages/ContactPage';
+import Layout from './Layout';
 
 function App() {
   return (
     <>
-      {/* <AboutPage /> */}
-      {/* <ContactPage /> */}
-      <CharactersPage />
-
-      {/* <h1>Marvel Characters</h1>
-      <CharactersList characters={characters} />
-      <br/>
-      <NumberOfCharacters characters={characters} /> */}
+      <Layout>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<CharactersPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </BrowserRouter>
+      </Layout>
     </>
   );
 }
