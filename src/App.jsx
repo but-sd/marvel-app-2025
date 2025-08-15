5043fb7f350c325ef6e85fa923921d3d228f836e
alexandre-girard <alexandre.girard@maif.fr>
2025-08-15 09:06:52 +0200

    Create CharactersList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import './App.css'
+import CharactersList from './components/CharactersList';
 import characters from './data/characters.json';
 
 function App() {
   return (
     <>
       <h1>Marvel Characters</h1>
-      <ul id="characters">
-        {characters.map((character) => (
-          <li key={character.id}>
-            {character.name}
-          </li>
-        ))}
-      </ul>
+      <CharactersList characters={characters} />
     </>
   );
 }
